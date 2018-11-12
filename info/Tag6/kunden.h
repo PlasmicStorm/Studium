8e1b15974e244ce7e80c7369bd32ee57c69ea91b
Nicolas Gassen <PlasmicStorm@Nicolas-MacBook.local>
2018-11-12 12:26:48 +0100

    kek

--- info/Tag6/kunden.h
+++
@@ -1,9 +0,0 @@
-#ifndef _KUNDEN_H
-#define _KUNDEN_H
-#include "kunden.h"
-
-void KundenAnlegen();
-void KundenBearbeiten();
-void KundenLoeschen();
-
-#endif
--- info/vorher/HelloWorld.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main (void) {
-	puts("hallu weld?");
-	return 0;
-}
